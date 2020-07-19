494a25c247b329ea068b0e8ca31bbb9e0fdf4b86
Manthan Mevada <manthanmevada66@gmail.com>
2020-07-19 21:12:33 +0530

    Added bootstrap calls

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const { app, BrowserWindow, Notification, session } = require('electron')
+const { app, BrowserWindow, session } = require('electron');
+const remote = require('electron').remote;
 const express = require('express');
 const expressApp = express();
 let request = require('request');
 const path = require('path');
 const dotenv = require('dotenv');
+const { cookie } = require('request');
 
 let win;
 
 let count = 0;
 
 dotenv.config();
+
+const countCookie = {
+    url: "http://localhost",
+    name: "pageCount",
+    value: "0"
+};
+
+function setCount(newCount) {
+    session.defaultSession.cookies.set({ url: "http://localhost", name: "pageCount", value: `${newCount}` }).then((result) => {
+        console.log("Kar di set iske saath " + newCount);
+    }).catch((err) => console.log("Smth happened"));
+}
+
+function getCount(callback) {
+    session.defaultSession.cookies.get({ name: "pageCount" }).then((cookies) => {
+        let intCount = parseInt(cookie[0].value);
+        console.log("Cookie mili get se " + intCount);
+        return callback(intCount);
+    }).catch((err) => {
+        console.log("Nahi mili cookie kyuki " + err);
+        session.defaultSession.cookies.set(countCookie);
+        return callback(0);
+    });
+}
 
 function getRandomQuote() {
     return new Promise((resolve, reject) => {
@@ -28,7 +54,11 @@ function getRandomQuote() {
                     if (Object.keys(quote).length == 0) {
                         resolve(JSON.parse('{"id": -1}'));
                     }
-                    count = count + 1;
+                    count += 1;
+                    getCount((count) => {
+                        console.log(`count is ${count}`);
+                        setCount(count);
+                    });
                     resolve(quote[0]);
                 }
             }
@@ -70,14 +100,15 @@ function reload() {
 
 function createWindow() {
     win = new BrowserWindow({
-        width: 850,
+        width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true
         }
     });
-    win.setMenu(null);
+    // win.setMenu(null);
     win.loadFile("./view/index.html");
+    getCount();
 }
 
 app.whenReady().then(createWindow);
