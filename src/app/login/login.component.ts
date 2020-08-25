409b2a8c36a622d02f828c776970255f9a005020
Manthan Mevada <manthanmevada66@gmail.com>
2020-08-25 09:12:28 +0530

    Added Google Sign In

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
+declare const gapi: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+
+  public auth2: any;
+  public googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: environment.CLIENT_URL,
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignIn(document.getElementById('gbutton'));
+    });
+  }
+  attachSignIn(element: HTMLElement) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      const profile = googleUser.getBasicProfile();
+      console.log('Name: ' + profile.getName());
+    }, (error) => {
+      alert(JSON.stringify(error, undefined, 2));
+    });
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.googleInit();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  TOKEN_SECRET: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWU5YTA2ZmViNWJhNDE2MjRkMDM2OWMiLCJpYXQiOjE1OTM3NTUzMTZ9.IJ7u8CmdZ3oZIn-WEdokQuapLtX5zwzoxzvJW9f0JSQ'
+  TOKEN_SECRET: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWU5YTA2ZmViNWJhNDE2MjRkMDM2OWMiLCJpYXQiOjE1OTM3NTUzMTZ9.IJ7u8CmdZ3oZIn-WEdokQuapLtX5zwzoxzvJW9f0JSQ',
+  CLIENT_URL: '609317059477-p723683roluef7rgnp3ddtvr9b6chgc3.apps.googleusercontent.com'
 };
 
 /*
